9a57452bddbc968b6860c8d015b56bc72fa188fd
JL8255 <j9660628@gmail.com>
2024-05-07 11:15:58 +0200

    refactor request

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,22 +1,21 @@
 const Book = require('../models/book');
 const fs = require('fs');
 
-exports.createBook = (req, res, next) => {
+exports.createBook = (req, res, next) => { // Création d'un livre dans la BD
     const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
+    // delete bookObject._id;
     delete bookObject._userId;
     const book = new Book({
         ...bookObject,
-        userId: req.auth.userId,
+        userId: req.auth.userId, // Récupère l'id de l'authentification
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-
     book.save()
         .then(() => { res.status(201).json({ message: 'Objet enregistré !' }) })
         .catch(error => { res.status(400).json({ error }) })
 };
 
-exports.modifyBook = (req, res, next) => {
+exports.modifyBook = (req, res, next) => { // MAJ d'un livre de la BD
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -38,7 +37,7 @@ exports.modifyBook = (req, res, next) => {
         });
 };
 
-exports.deleteBook = (req, res, next) => {
+exports.deleteBook = (req, res, next) => { // Efface un livre de la BD
     Book.findOne({ _id: req.params.id })
         .then(book => {
             if (book.userId != req.auth.userId) {
@@ -67,4 +66,12 @@ exports.getAllBooks = (req, res, next) => {  // Renvoie un tableau de tous les l
     Book.find()
         .then(books => res.status(200).json(books))
         .catch(error => res.status(400).json({ error }));
+};
+
+exports.getBeastrating = (req, res, next) => { // Renvoie les 3 livres les ayant la meilleure note moyenne
+
+};
+
+exports.rateABook = (req, res, next) => {  // Donne une note à un livre
+
 };
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -5,9 +5,12 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer.config')
 
 router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/:id/rating', auth, bookCtrl.rateABook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
-router.get('/:id', auth, bookCtrl.getOneBook);
-router.get('/', auth, bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllBooks);
+router.get('/beastrating', bookCtrl.getBeastrating);
+
 
 module.exports = router;
